9a67642aaeaf5cd9e9f4149a4bda03648be9d6b4
Klaudiusz Kołtuński <koltunski.klaudiusz@gmail.com>
2022-11-02 22:46:04 +0100

    exercise3

---
+++ exercise3/app/templates_c/d6255a89f667d59b981de735d788cef91657dff2_0.file.main.html.php
@@ -0,0 +1,147 @@
+<?php
+/* Smarty version 4.2.1, created on 2022-11-02 22:34:58
+  from 'C:\xampp80\htdocs\NetworkApplicationsLaboratory\exercise3\templates\main.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.2.1',
+  'unifunc' => 'content_6362e2822458d4_64279640',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd6255a89f667d59b981de735d788cef91657dff2' => 
+    array (
+      0 => 'C:\\xampp80\\htdocs\\NetworkApplicationsLaboratory\\exercise3\\templates\\main.html',
+      1 => 1667424501,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6362e2822458d4_64279640 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (($tmp = $_smarty_tpl->tpl_vars['page_description']->value ?? null)===null||$tmp==='' ? "Opis domyślny" ?? null : $tmp);?>
+">
+	<meta name="author"      content="Klaudiusz Kołtuński">
+
+	<link rel="shortcut icon" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/assets/images/gt_favicon.png">
+	
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/assets/css/main.css">
+</head>
+
+<body class="home">
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li class="active"><a href="#">Top page</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div> 
+	<!-- /.navbar -->
+
+	<!-- Header -->
+	<header id="head">
+		<h1 class="lead"><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_title']->value ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</h1>
+		<p class="tagline"><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_description']->value ?? null)===null||$tmp==='' ? "Opis domyślny" ?? null : $tmp);?>
+</p>
+	</header>
+	<!-- /Header -->
+		
+	<div class="jumbotron top-space">
+    	<div id="app_content" class="container">
+			<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_14198213486362e282244b37_42011306', 'content');
+?>
+
+		</div>
+	</div>
+
+	<div class="footer2 top-space">
+		<div class="container">
+			<div class="row">
+				
+				<div class="col-md-6 widget">
+					<div class="widget-body">
+						<p class="simplenav">
+							<a href="#">Top page</a> 
+						</p>
+					</div>
+				</div>
+
+				<div class="col-md-6 widget">
+					<div class="widget-body">
+						<p class="text-right">
+							Copyright &copy; 2022, Klaudiusz Kołtuński. Designed by <a href="http://gettemplate.com/" rel="designer">gettemplate</a> 
+						</p>
+					</div>
+				</div>
+
+			</div> 
+		</div>
+	</div>
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<?php echo '<script'; ?>
+ src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"><?php echo '</script'; ?>
+>
+	<?php echo '<script'; ?>
+ src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/assets/js/headroom.min.js"><?php echo '</script'; ?>
+>
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/assets/js/jQuery.headroom.min.js"><?php echo '</script'; ?>
+>
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/assets/js/template.js"><?php echo '</script'; ?>
+>
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_14198213486362e282244b37_42011306 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_14198213486362e282244b37_42011306',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+}
---
+++ exercise3/templates_c/13d1c8ed2673e45ed6de46cfc4ff96eb5d167131_0.file.creditCalc.html.php
@@ -0,0 +1,139 @@
+<?php
+/* Smarty version 4.2.1, created on 2022-11-02 22:21:40
+  from 'C:\xampp80\htdocs\NetworkApplicationsLaboratory\exercise3\app\creditCalc.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.2.1',
+  'unifunc' => 'content_6362df64e58304_59403770',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '13d1c8ed2673e45ed6de46cfc4ff96eb5d167131' => 
+    array (
+      0 => 'C:\\xampp80\\htdocs\\NetworkApplicationsLaboratory\\exercise3\\app\\creditCalc.html',
+      1 => 1667424053,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6362df64e58304_59403770 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1160929056362df64e4af19_27671365', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_4898321276362df64e4b834_67763465', 'content');
+$_smarty_tpl->inheritance->endChild($_smarty_tpl, '..\templates\main.html');
+}
+/* {block 'footer'} */
+class Block_1160929056362df64e4af19_27671365 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'footer' => 
+  array (
+    0 => 'Block_1160929056362df64e4af19_27671365',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+przykładowa tresć stopki wpisana do szablonu głównego z szablonu kalkulatora<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_4898321276362df64e4b834_67763465 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_4898321276362df64e4b834_67763465',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+\app\creditCalc.php" method="post">
+		<fieldset>
+
+            <label for="id_x">Amount: </label>
+            <input id="id_x" type="text" name="x" value="<?php echo $_smarty_tpl->tpl_vars['form']->value['x'];?>
+" /><br />
+            <label for="id_y">Years count: </label>
+            <input id="id_y" type="text" name="y" value="<?php echo $_smarty_tpl->tpl_vars['form']->value['y'];?>
+" /><br />
+            <label for="id_z">Interest rate: </label>
+            <input id="id_z" type="text" name="z" value="<?php echo $_smarty_tpl->tpl_vars['form']->value['z'];?>
+" /><br />
+            <input type="submit" value="Check" />
+
+		</fieldset>
+	</form>
+</div>
+
+<?php if ((isset($_smarty_tpl->tpl_vars['messages']->value))) {?>
+	<?php if (count($_smarty_tpl->tpl_vars['messages']->value) > 0) {?> 
+		<h4>Errors: </h4>
+		<ol class="err">
+		<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['messages']->value, 'msg');
+$_smarty_tpl->tpl_vars['msg']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+$_smarty_tpl->tpl_vars['msg']->do_else = false;
+?>
+		<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</li>
+		<?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+		</ol>
+	<?php }
+}?>
+
+<?php if ((isset($_smarty_tpl->tpl_vars['infos']->value))) {?>
+	<?php if (count($_smarty_tpl->tpl_vars['infos']->value) > 0) {?> 
+		<h4>Infos: </h4>
+		<ol class="inf">
+		<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['infos']->value, 'msg');
+$_smarty_tpl->tpl_vars['msg']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+$_smarty_tpl->tpl_vars['msg']->do_else = false;
+?>
+		<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</li>
+		<?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+		</ol>
+	<?php }
+}?>
+
+<?php if ((isset($_smarty_tpl->tpl_vars['result']->value))) {?>
+	<h4>Monthly:</h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['result']->value;?>
+
+	</p>
+<?php }?>
+
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
