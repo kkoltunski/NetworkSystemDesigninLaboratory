33521bc598d313ab8aa305848d53034c7a4a0323
Klaudiusz Kołtuński <koltunski.klaudiusz@gmail.com>
2022-11-14 08:55:55 +0100

    exercise6

---
+++ exercise6/ctrl.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'init.php';
+
+getRouter()->setDefaultRoute('calcShow');
+getRouter()->setLoginRoute('login');
+
+getRouter()->addRoute('calcShow', 'CreditCalcCtrl', ['user','admin']);
+getRouter()->addRoute('calcCompute', 'CreditCalcCtrl', ['user','admin']);
+getRouter()->addRoute('login', 'LoginCtrl');
+getRouter()->addRoute('logout', 'LoginCtrl', ['user','admin']);
+
+getRouter()->go();
